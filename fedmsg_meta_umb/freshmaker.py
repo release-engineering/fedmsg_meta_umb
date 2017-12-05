8725d8a294525e379da5529e2e5f266e44ce66ab
Chenxiong Qi <cqi@redhat.com>
2017-12-05 19:05:59 +0800

    Processor for freshmaker events
    Signed-off-by: Chenxiong Qi <cqi@redhat.com>

---
+++ fedmsg_meta_umb/freshmaker.py
@@ -0,0 +1,72 @@
+# Copyright (C) 2017 Red Hat, Inc.
+#
+# fedmsg_meta_umb is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# fedmsg_meta_umb is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with fedmsg; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Authors:  Chenxiong Qi <cqi@redhat.com>
+
+from fedmsg.meta.base import BaseProcessor
+
+
+class FreshmakerProcessor(BaseProcessor):
+    topic_prefix_re = r'/topic/VirtualTopic\.eng'
+
+    __name__ = 'freshmaker'
+    __description__ = 'a service scheduling rebuilds of artifacts as new content becomes available'
+    __link__ = 'https://freshmaker.engineering.redhat.com'
+    __docs__ = 'https://mojo.redhat.com/docs/DOC-1155261'
+    __obj__ = 'Artifact rebuild scheduler'
+
+    def title(self, msg, **config):
+        return msg['topic'].split('.', 2)[-1]
+
+    def subtitle(self, msg, **config):
+        inner_msg = msg['msg']
+        topic = msg['topic']
+
+        if topic.endswith('manual.rebuild'):
+            template = self._('Manual rebuild is triggered for Errata advisory {errata_id}.')
+        elif topic.endswith('build.state.changed'):
+            if inner_msg['state_name'] == 'DONE':
+                template = self._('{type_name} {name} is successfully rebuilt '
+                                  'from original build {original_nvr} to new '
+                                  'build {rebuilt_nvr}.')
+            else:
+                template = self._('{type_name} build state is switched to {state_name} '
+                                  'due to "{state_reason}"')
+        elif topic.endswith('event.state.changed'):
+            template = self._('Event state is switched to {state_name} due to "{state_reason}"')
+        else:
+            template = 'Unknown message.'
+
+        return template.format(**inner_msg)
+
+    def link(self, msg, **config):
+        topic = msg['topic']
+
+        if (topic.endswith('build.state.changed') or
+                topic.endswith('event.state.changed')):
+            return msg['msg']['url']
+        elif topic.endswith('manual.rebuild'):
+            return 'https://errata.devel.redhat.com/advisory/{0}'.format(
+                msg['msg']['errata_id'])
+        else:
+            # For any other new kind of message that is not known yet
+            return None
+
+    def packages(self, msg, **config):
+        if msg['topic'].endswith('event.state.changed'):
+            return set([nvr.rsplit('-', 2)[0] for nvr in msg['msg']['builds']])
+        else:
+            return set([])
